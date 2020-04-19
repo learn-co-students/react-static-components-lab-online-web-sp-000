65af1ac0e8f94fc8d49774993934fbe712e66bf9
oliverpdahl <oliverpdahl@gmail.com>
2020-04-19 08:36:10 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import GraceHopperQuoteComponent from './GraceHopperQuoteComponent'
+import CatComponent from './CatComponent'
+import MouseEvent from './MouseComponent'
 
 class App extends Component {
 	render() {
@@ -7,6 +10,7 @@ class App extends Component {
 			<div className="App">
 				<CatComponent />
 				<GraceHopperQuoteComponent />
+        <MouseEvent />
 				{/* one more component missing */}
 			</div>
 		);
