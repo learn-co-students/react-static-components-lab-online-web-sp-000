51a9fe9fd963be73344608bae6e7fb526fcb3889
Lindsey Wells <github email address>
2020-05-14 20:46:00 +0100

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -9,32 +9,32 @@ import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('App', () => {
-	let wrapper;
+    let wrapper;
 
-	before(() => {
-		wrapper = shallow(React.createElement(App));
-	});
+    before(() => {
+        wrapper = shallow(React.createElement(App));
+    });
 
-	it('should include the cat component', () => {
-		expect(wrapper.text()).to.include('<CatComponent />');
-	});
+    it('should include the cat component', () => {
+        expect(wrapper.text()).to.include('<CatComponent />');
+    });
 
-	it('should include the einstein-quote component', () => {
-		expect(wrapper.text()).to.include('<EinsteinQuoteComponent />');
-	});
+    it('should include the grace-hopper-quote component', () => {
+        expect(wrapper.text()).to.include('<GraceHopperQuoteComponent />');
+    });
 
-	it('should include the mouse component', () => {
-		expect(wrapper.text()).to.include('<MouseComponent />');
-	});
+    it('should include the mouse component', () => {
+        expect(wrapper.text()).to.include('<MouseComponent />');
+    });
 
-	it('should render each component once', () => {
-		expect(wrapper.children()).to.have.length(3);
-	});
+    it('should render each component once', () => {
+        expect(wrapper.children()).to.have.length(3);
+    });
 
-	it('should render the components in the proper order', () => {
-		const [first, second, third] = wrapper.children();
-		expect(first.type.name === 'CatComponent').to.be.true;
-		expect(second.type.name === 'EinsteinQuoteComponent').to.be.true;
-		expect(third.type.name === 'MouseComponent').to.be.true;
-	});
+    it('should render the components in the proper order', () => {
+        const [first, second, third] = wrapper.children();
+        expect(first.type.name === 'CatComponent').to.be.true;
+        expect(second.type.name === 'GraceHopperQuoteComponent').to.be.true;
+        expect(third.type.name === 'MouseComponent').to.be.true;
+    });
 });
